2eb5703cc796df815fddc70aab1b56ae85d565af
Matt Gordon <matt@mattrgordon.com>
2024-06-09 22:50:56 -0500

    got daily forecasts to render after looping through 3 hour increments each day

--- assets/js/script.js
+++ assets/js/script.js
@@ -74,36 +74,44 @@ function getCityWeather(lat, long) {
     Promise.all([currentWeatherFetch, forecastWeatherFetch])
       .then(([currentWeatherResponse, forecastWeatherResponse]) => {
         const currentWeather = currentWeatherResponse.data;
-        const forecastData = forecastWeatherResponse.data;
-        console.log('weather info', currentWeather, forecastData);
+        const forecastData = forecastWeatherResponse.data.list;
+        const forecastArr = [];
+        for (let i = 0; i < forecastData.length; i += 8) {
+          const item = {
+            date: dayjs(forecastData[i].dt * 1000).format('MM/DD/YYYY'),
+            icon: forecastData[i].weather[0].icon,
+            temp: forecastData[i].main.temp,
+            wind: forecastData[i].wind.speed,
+            humidity: forecastData[i].main.humidity,
+          };
+          forecastArr.push(item);
+        }
+        setWeatherData({
+          temp: currentWeather.main.temp,
+          wind: currentWeather.wind.speed,
+          humidity: currentWeather.main.humidity,
+          name: currentWeather.name,
+          icon: currentWeather.weather[0].icon,
+          status: currentWeather.weather[0].main,
+          forecast: forecastArr,
+        });
       })
       .catch((error) => {
         console.error('There was an error fetching', error);
       });
-    //   .then(function (response) {
-    //     return response.json();
-    //   })
-    //   .then(function (data) {
-    //     console.log('local weather', data);
-    //     getLocalForecast(lat, long);
-    //     setWeatherData({
-    //       temp: data.main.temp,
-    //       wind: data.wind.speed,
-    //       humidity: data.main.humidity,
-    //       name: data.name,
-    //       icon: data.weather[0].icon,
-    //       status: data.weather[0].main,
-    //     });
-    //   });
   }
 }
 
 function setWeatherData(data) {
   console.log('weather data', data);
   const localWeatherData = document.getElementById('localWeatherData');
+  const localWeatherForecast = document.getElementById('localWeatherForecast');
+  const forecastData = data.forecast;
   localWeatherData.innerHTML = `
-    <h2 class="is-size-4">${data.name} (6/7/2024)</h2>
-    <img src="https://openweathermap.org/img/wn/${data.icon}.png" />${data.status}
+    <h2 class="is-size-4">${data.name} (${dayjs().format('MM/DD/YYYY')})</h2>
+    <img src="https://openweathermap.org/img/wn/${data.icon}.png" />${
+    data.status
+  }
     <div class="columns">
         <div class="column is-one-quarter">
             <p>Temp: ${data.temp}ºF</p>
@@ -120,34 +128,23 @@ function setWeatherData(data) {
         </div>
     </div>
   `;
-}
-
-function getLocalForcast(lat, long) {
-  if (lat && long) {
-    fetch(
-      `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&units=imperial&appid=${openWeatherAPIKey}`
-    )
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log('local forecast', data);
-        setForecastData({
-          temp: data.main.temp,
-          wind: data.wind.speed,
-          humidity: data.main.humidity,
-          name: data.name,
-          icon: data.weather[0].icon,
-          status: data.weather[0].main,
-        });
-      });
+  for (let i = 0; i < forecastData.length; i++) {
+    const item = forecastData[i];
+    const div = document.createElement('div');
+    div.setAttribute('class', 'column');
+    div.innerHTML = `
+    <section
+      class="box has-background-primary-40 has-text-primary-55-invert"
+    >
+      <h3>${item.date}</h3>
+      <img src="https://openweathermap.org/img/wn/${item.icon}.png" />
+      <p>Temp: ${item.temp}ºF</p>
+      <p>Wind: ${item.wind} MPH</p>
+      <p>${item.humidity}%</p>
+    </section>
+  `;
+    localWeatherForecast.append(div);
   }
-}
-
-function setForecastData(data) {
-  console.log('forecast data', data);
-  const localWeatherForecast = document.getElementById('localWeatherForecast');
-  localWeatherForecast.innerHTML = '';
 }
 
 // builds out the list of searched cities
@@ -157,7 +154,9 @@ function setCities() {
   for (let i = 0; i < savedCities.length; i++) {
     const li = document.createElement('li');
     const button = document.createElement('button');
-    button.setAttribute('id', `${savedCities[i].name}Button`);
+    const id = `${savedCities[i].name}Button`;
+    console.log('id', id);
+    button.setAttribute('id', id);
     button.setAttribute(
       'class',
       'button is-info is-inverted is-fullwidth my-4'
@@ -165,6 +164,10 @@ function setCities() {
     button.textContent = savedCities[i].name;
     li.appendChild(button);
     savedLocations.appendChild(li);
+    id.addEventListener('click', function (event) {
+      event.preventDefault();
+      getLatLong(savedCities[i].name);
+    });
   }
 }
 
@@ -175,3 +178,4 @@ function init() {
 }
 
 init();
+getLatLong('minneapolis');
